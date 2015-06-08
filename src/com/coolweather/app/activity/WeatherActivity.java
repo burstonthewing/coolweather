0ac598cdbaa06b307e36be6180dc3bd90c6fa047
burst <burstonthewing@163.com>
2015-06-08 11:11:12 +0800

    增加后台自动更新天气功能，并修改app图标及名称

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.coolweather.app.activity;
 
 import com.coolweather.app.R;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -160,5 +161,9 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		
+		//开启自动更新服务
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 }
